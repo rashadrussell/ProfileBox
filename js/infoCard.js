54991dd04e8e9067b4c5599061a1e2aa89543b69
Rashad Russell <rashad.j.russell@gmail.com>
2013-01-09 20:55:00 -0600

    Commit

--- js/infoCard.js
+++ js/infoCard.js
@@ -1,72 +1,96 @@
+(function() {
+
 // Create InfoBubble constructor
 // infoList parameter gets an unordered list DOM element
-function InfoBubble(infoList) {
+  InfoCards = function (infoList) {
 
-    var list = infoList;
+      this.list = document.getElementById(infoList.list);
+      console.log(this.list);
+  };
 
-    getList = function(){
-      return list;
-    }
+  InfoCards.prototype = {
+    
+    /*
+     *  Initialize InfoCard
+     */
+        init : function() {
 
-}
+          this.getInfo();
+          this.popup();
 
-/* 
-Create "setTransparentOverlay" static method to place a transparent screen to cover
-the page when an li element is clicked.
-*/
-InfoBubble.setTransparentOverlay = function() {
-  $("body").prepend('<div id="transparentOverlay"></div>');
+        },
 
-  $("#transparentOverlay").bind("click", function(){
-    $("#transparentOverlay").remove();
-    $("#bubble").remove();
-  });
-}
+    /*
+     *  Create the "getInfo" instance method. This method gathers the profile information of each
+     *  members' li element.
+     */
+        getInfo : function() {
 
-/*
-Create "bubble" static method. This method creates the profile card.
-*/
-InfoBubble.bubble = function(info) {
-  $("body").append('<div id="bubble">' + info +'</div>');
+          $(this.list).find("li").bind("click", function() {
 
-  var bubbleWidth = $("#bubble").width();
-  var windowWidth = $(window).width();
-  var centerAlign = parseInt(windowWidth/2) - parseInt(bubbleWidth/2);
+              var info = $(this).find("#profile_info").html();
+    
+          });
 
-  $("#bubble").css("margin-left", centerAlign);
-        $("body").find('#bubble').animate({
+        },
+
+        /*
+         *  Create "popup" instance method. This method combines the "setTransparentOverlay" and
+         *  the "bubble" methods to create the entire effect.
+         */
+        popup : function() {
+
+          /* 
+           *  Create "setTransparentOverlay" static method to place a transparent screen to cover
+           *  the page when an li element is clicked.
+           */
+           function setTransparentOverlay() {
+
+            $("body").prepend('<div id="transparentOverlay"></div>');
+
+            $("#transparentOverlay").bind("click", function() {
+
+              $("#transparentOverlay").remove();
+              $("#bubble").remove();
+
+            });
+
+           };
+
+          /*
+           *  Create "bubble" static method. This method creates the profile card.
+           */
+           function bubble(info) {
+
+            $("body").append('<div id="bubble">' + info +'</div>');
+
+            var bubbleWidth = $("#bubble").width();
+            var windowWidth = $(window).width();
+            var centerAlign = parseInt(windowWidth/2) - parseInt(bubbleWidth/2);
+
+            $("#bubble").css("margin-left", centerAlign);
+
+            $("body").find('#bubble').animate({
               height: 320,
               width: 320,
               "top": "-10",
               "right": "10",
               "bottom": "10",
               "left": "-10"
-        },200);
-}
+            },200);
 
-/*
-Create the "getInfo" instance method. This method gathers the profile information of each
-members' li element.
-*/
-InfoBubble.prototype.getInfo = function(){
+           };
 
-  $(getList()).find("li").bind("click", function(){
+           $(this.list).find("li").bind("click", function() {
 
-      var info = $(this).find("#profile_info").html();
-    
-  });
+            setTransparentOverlay();
+            bubble($(this).find("#profile_info").html());    
 
-}
+          });
 
-/*
-Create "popup" instance method. This method combines the "setTransparentOverlay" and
-the "bubble" methods to create the entire effect.
- */
-InfoBubble.prototype.popup = function() {
-  $(getList()).find("li").bind("click", function(){
+        }
 
-    InfoBubble.setTransparentOverlay();
-    InfoBubble.bubble($(this).find("#profile_info").html());    
+  };
 
-  });
-}
+
+}());
